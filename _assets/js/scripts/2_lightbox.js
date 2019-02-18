8a061301a633948e8f92e2b539e99ddd72e4a27f
justin.laxamana <justrlax@gmail.com>
2019-02-18 14:13:16 -0600

    Update

--- _assets/js/scripts/2_lightbox.js
+++ _assets/js/scripts/2_lightbox.js
@@ -123,8 +123,3 @@ $('.alert').magnificPopup({
     }
   ]
 });
-//Mobile Hover For Preview
-$('.touch .grid-section__item .image').on("click", function(e){
-  $(this).toggleClass('active');
-  e.preventDefault();
-});
